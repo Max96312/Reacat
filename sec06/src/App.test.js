0f24f810739be1e4b6d25dc069ec034f2fc115ad
Max96312 <k0mp4ny16@gmail.com>
2024-08-07 20:22:51 +0900

    sec06

---
+++ sec06/src/App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.queryByText(/learn react/i);
+
+  // getBy + waitFor
+  // const linkElement = screen.findByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+
+  const lintTest = screen.getByRole('button', {
+    name: 'lintTest'
+  })
+
+  expect(lintTest.textContent).toBe('lintTest')
+});
