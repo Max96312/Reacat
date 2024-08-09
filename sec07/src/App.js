6d25ffaa5c08f5a4c5977817dbbdcdf579c4243d
Max96312 <k0mp4ny16@gmail.com>
2024-08-09 22:56:42 +0900

    sec05-1

---
+++ sec07/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testId="counter">{count}</h3>
+        <button data-testId="minus-button" onClick={() => setCount(prev => prev -1)}>-</button>
+        <button data-testId="plus-button" onClick={() => setCount(prev => prev + 1)}>+</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
