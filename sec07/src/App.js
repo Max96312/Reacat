4d24a6d82ff0f37e2ae35a03131d8ba500fefc5e
Max96312 <k0mp4ny16@gmail.com>
2024-08-09 23:43:34 +0900

    sec05

--- sec07/src/App.js
+++ sec07/src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [disabled, setDisabled] = useState(false)
+  
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testId="counter">{count}</h3>
-        <button data-testId="minus-button" onClick={() => setCount(prev => prev -1)}>-</button>
-        <button data-testId="plus-button" onClick={() => setCount(prev => prev + 1)}>+</button>
+        <button data-testId="minus-button" onClick={() => setCount(prev => prev -1)} disabled={disabled}>-</button>
+        <button data-testId="plus-button" onClick={() => setCount(prev => prev + 1)} disabled={disabled}>+</button>
+        <div>
+          <button
+            data-testId="on/off-button"
+            style={{backgroundColor: 'blue'}}
+            onClick={() => setDisabled(prev => !prev)}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
