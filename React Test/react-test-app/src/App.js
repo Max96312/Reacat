1d9e8dfd6efee8f4fcc95ceaa64ce329109f26c4
Max96312 <k0mp4ny16@gmail.com>
2024-08-23 23:38:02 +0900

    sec02

--- React Test/react-test-app/src/App.js
+++ React Test/react-test-app/src/App.js
@@ -1,13 +1,38 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            onClick={() => setCounter((counter) => counter - 1)}
+            disabled={disabled}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCounter((counter) => counter + 1)}
+            disabled={disabled}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
