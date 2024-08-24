0aad3baf5544e1a22d0648b952d49b0c699dd69f
Max96312 <k0mp4ny16@gmail.com>
2024-08-24 23:53:13 +0900

    sec05

--- React Test/react-shop-test-app-starter/src/App.test.js
+++ React Test/react-shop-test-app-starter/src/App.test.js
@@ -1,5 +1,10 @@
 import { render } from "@testing-library/react";
 import App from "./App";
-test("Test Start", async () => {
+test("", async () => {
   render(<App />);
+  const lintTest = screen.getByRole("button", {
+    name: "lintTest"
+  })
+
+  expect(lintTest.testContent).toBe('lintTest');
 });
