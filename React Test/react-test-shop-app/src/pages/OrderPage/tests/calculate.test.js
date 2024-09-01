20421a76f787381fe7c7382082e8548c4581d5b6
Max96312 <k0mp4ny16@gmail.com>
2024-09-01 23:14:31 +0900

    sec06: calculate 테스트 중 fail 발생

--- React Test/react-test-shop-app/src/pages/OrderPage/tests/calculate.test.js
+++ React Test/react-test-shop-app/src/pages/OrderPage/tests/calculate.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Type from "../Type";
+import { OrderContextProvider } from "../../../contexts/OrderContext";
 
 test("update product's total when products change", () => {
-  render(<Type orderType="products" />);
+  render(<Type orderType="products" />, { wrapper: OrderContextProvider });
 
-  const productsTotal = screen.getByText("상품 총 가격:", { exact: false });
+  const productsTotal = screen.getByText("총 가격:", { exact: false });
   expect(productsTotal).toHaveTextContent("0");
 
   //   아메리카 여행 상품 1개 올리기
